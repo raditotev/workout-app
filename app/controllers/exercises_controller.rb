7055e72098b271880e0cc6ac180076ce303cb404
raditotev <raditotev@gmail.com>
2016-11-30 18:36:37 +0000

    Add routes, exercises controller with index and new actions

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,9 @@
+class ExercisesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @exercise = current_user.exercises.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'dashboards#index'
+
+  resources :users do
+    resources :exercises
+  end
 end
