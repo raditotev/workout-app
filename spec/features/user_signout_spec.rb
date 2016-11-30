fe4f32b8bb2df5aba88d868c679fcbf6ad63d4b6
raditotev <raditotev@gmail.com>
2016-11-30 16:26:25 +0000

    Add sign out

---
+++ spec/features/user_signout_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "User Sign Out" do
+
+  before do
+    @john = User.create!(email: "john@example.com", password: "password")
+    visit new_user_session_path
+    fill_in "Email", with: @john.email
+    fill_in "Password", with: @john.password
+    click_button "Log in"
+  end
+
+  scenario "with valid credentials" do
+    visit "/"
+    click_link "Sign out"
+
+    expect(page).to have_content "Signed out successfully."
+    # expect(page).not_to have_content "SIgned in as #{@john.email}"
+  end
+
+end
