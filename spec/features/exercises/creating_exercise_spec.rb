dfbc21d55b1e526e75ba0e393667041aa0870ee6
raditotev <raditotev@gmail.com>
2016-11-30 18:19:07 +0000

    Include devise helper methods in rspec

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Creating Exercise" do
 
   before do
     @john = User.create!(email: "john@example.com", password: "password")
-    login_as @john
+    login_as(@john)
   end
 
   scenario "with valid input " do
