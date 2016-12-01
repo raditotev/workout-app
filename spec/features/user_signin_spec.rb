ac895620293460e486472ff727ee322fef94f8ea
raditotev <raditotev@gmail.com>
2016-12-01 19:20:56 +0000

    Add user names in failing feat specs

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "User Sign  In" do
 
   before do
-    @john = User.create!(email: "john@example.com", password: "password")
+    @john = User.create!(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
   end
 
   scenario "with valid credentials" do
--- spec/features/user_signout_spec.rb
+++ spec/features/user_signout_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "User Sign Out" do
 
   before do
-    @john = User.create!(email: "john@example.com", password: "password")
+    @john = User.create!(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
     visit new_user_session_path
     fill_in "Email", with: @john.email
     fill_in "Password", with: @john.password
