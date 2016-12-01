831683e792a5e295724c79288cb2d64df8b7a255
raditotev <raditotev@gmail.com>
2016-12-01 13:13:14 +0000

    Update create exercise feat spec

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -17,7 +17,7 @@ RSpec.feature "Creating Exercise" do
 
     fill_in "Duration", with: 70
     fill_in "Workout details", with: "Weight lifting"
-    fill_in "Activity date", with: "2016-07-26"
+    fill_in "Activity date", with: 3.days.ago
     click_button "Create Exercise"
 
     expect(page).to have_content "Exercise has been created"
