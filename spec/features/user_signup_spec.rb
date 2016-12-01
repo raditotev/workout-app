18c66d610a744fd38fe10e5d09090363787c6e59
raditotev <raditotev@gmail.com>
2016-12-01 19:08:36 +0000

    Add user names on registration

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -6,6 +6,8 @@ RSpec.feature "User Signup" do
     visit "/"
     click_link "Sign up"
 
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
     fill_in "Email", with: "john@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
@@ -15,5 +17,8 @@ RSpec.feature "User Signup" do
     expect(page).to have_content "You have signed up successfully."
     expect(page).not_to have_link "Sign in"
     expect(page).not_to have_link "Sign up"
+
+    visit "/"
+    expect(page).to have_content "John Doe"
   end
 end
